flatpak/flatpak-builder-tools

commit 47ce4294195557c92b0e91cf4c2ca0ac80ba11b9

node: make it work with git+https urls
The generator produced wrongly looking URLs for git+https:// URLs:

Initialized empty Git repository in
/srv/buildbot/sources/git/https_github.com_Simon-Laux_parcel-plugin-clean-dist.git-JT0KG0/
fatal: unable to access
'https:////github.com/Simon-Laux/parcel-plugin-clean-dist.git/': Could
not resolve host: https; Unknown error
Failed to download sources: module delta: Child process exited with code
128

Notice how there are too many slashes.

Do also note how the behaviour should change now. The code suggests that
a URL starting with "git+https:" is going to be replaced with
"https://". This change changes this behaviour and strips the slashes
from the replacement. I hope that doesn't break any other users.

node/flatpak-node-generator.py
@@ -508,12 +508,12 @@ class NpmLockfileProvider(LockfileProvider):
 
     def parse_git_source(self, version: str, from_: str) -> Optional[GitSource]:
         git_prefixes = {
-            'github': 'https://github.com/',
-            'gitlab': 'https://gitlab.com/',
-            'bitbucket': 'https://bitbucket.com/',
-            'git': 'git://',
-            'git+http': 'http://',
-            'git+https': 'https://',
+            'github:': 'https://github.com/',
+            'gitlab:': 'https://gitlab.com/',
+            'bitbucket:': 'https://bitbucket.com/',
+            'git:': 'git://',
+            'git+http:': 'http:',
+            'git+https:': 'https:',
         }
 
         assert version.count('#') == 1, version
@@ -522,7 +522,7 @@ class NpmLockfileProvider(LockfileProvider):
         url: Optional[str] = None
 
         for npm_prefix, url_prefix in git_prefixes.items():
-            if original.startswith(npm_prefix + ':'):
+            if original.startswith(npm_prefix):
                 url = url_prefix + original[len(npm_prefix)+1:]
                 break
         else:

commit 2784eada77d53448df8fee2375e2545bfcfd2d79

node: Don't strip the first character of Git repos
This was broken by 47ce4294195557c92b0e91cf4c2ca0ac80ba11b9, where
the colon prefix was moved into the git_prefixes dict, but
len(prefix) + 1 characters were still being stripped off.

node/flatpak-node-generator.py
@@ -851,7 +851,7 @@ class NpmLockfileProvider(LockfileProvider):
 
         for npm_prefix, url_prefix in git_prefixes.items():
             if original.startswith(npm_prefix):
-                url = url_prefix + original[len(npm_prefix) + 1:]
+                url = url_prefix + original[len(npm_prefix):]
                 break
         else:
             return None