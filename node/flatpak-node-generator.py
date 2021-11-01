92f967c592289db660c6c2c832ac3679a43a4fd5
catsout <outline941@live.com>
2021-11-02 07:15:12 +0800

    node: Fix npm git patch from_ match

--- node/flatpak-node-generator.py
+++ node/flatpak-node-generator.py
@@ -1377,7 +1377,8 @@ class NpmModuleProvider(ModuleProvider):
                     original_version = f'{source.original}'
                     new_version = f'{path}#{source.commit}'
                     assert source.from_ is not None
-                    data['package.json'][source.from_] = new_version
+                    from_ = re.sub(r'^[^@]+@git\+', 'git+', source.from_)
+                    data['package.json'][from_] = new_version
                     data['package-lock.json'][original_version] = new_version
 
                 for filename, script in scripts.items():
