fdbf8e94e2db125fc09a1d70b2bb835d56950d96
Meng-Yuan Huang <myh@live.com>
2022-05-26 10:08:16 +0800

    node: Fix support for Electron v19+.

--- node/flatpak-node-generator.py
+++ node/flatpak-node-generator.py
@@ -851,6 +851,10 @@ class ElectronBinaryManager:
 
     def find_binaries(self, binary: str) -> Iterator['ElectronBinaryManager.Binary']:
         for electron_arch, flatpak_arch in self.ELECTRON_ARCHES_TO_FLATPAK.items():
+            # Electron v19+ drop linux-ia32 support.
+            if SemVer.parse(self.version) >= SemVer.parse("19.0.0") and electron_arch == "ia32":
+                continue
+
             binary_filename = f'{binary}-v{self.version}-linux-{electron_arch}.zip'
             binary_url = self.child_url(binary_filename)
 
