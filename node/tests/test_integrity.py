be22317a6bf96ace3f1b55f5cd389463c95efffc
Ryan Gonzalez <ryan.gonzalez@collabora.com>
2022-06-10 17:11:12 -0500

    node: Add tests for parsing base64 integrity values
    Signed-off-by: Ryan Gonzalez <ryan.gonzalez@collabora.com>

--- node/tests/test_integrity.py
+++ node/tests/test_integrity.py
@@ -3,6 +3,7 @@ from flatpak_node_generator.integrity import Integrity, IntegrityBuilder
 TEST_STRING = 'this is a test string'
 TEST_SHA1 = '9a375f77abb15794900c2689812204273d757c9b'
 TEST_SHA256 = 'f6774519d1c7a3389ef327e9c04766b999db8cdfb85d1346c471ee86d65885bc'
+TEST_SHA256_B64 = '9ndFGdHHozie8yfpwEdmuZnbjN+4XRNGxHHuhtZYhbw='
 
 
 def test_generate() -> None:
@@ -23,5 +24,11 @@ def test_builder() -> None:
         builder.update(TEST_STRING[i : i + STEP_SIZE])
 
     integrity = builder.build()
+    assert integrity.algorithm == 'sha256'
+    assert integrity.digest == TEST_SHA256
+
+
+def test_parse() -> None:
+    integrity = Integrity.parse(f'sha256-{TEST_SHA256_B64}')
     assert integrity.algorithm == 'sha256'
     assert integrity.digest == TEST_SHA256
