aef0619ca3e2e8655fc7a0d1127a12d19d3e1d4a
Ryan Gonzalez <ryan.gonzalez@collabora.com>
2022-06-15 19:20:55 -0500

    node: Clean up some strange / unneeded code
    Signed-off-by: Ryan Gonzalez <ryan.gonzalez@collabora.com>

--- node/flatpak_node_generator/providers/npm.py
+++ node/flatpak_node_generator/providers/npm.py
@@ -318,11 +318,10 @@ class NpmModuleProvider(ModuleProvider):
                 }
 
                 for path, source in sources.items():
-                    original_version = f'{source.original}'
                     new_version = f'{path}#{source.commit}'
                     assert source.from_ is not None
                     data['package.json'][source.from_] = new_version
-                    data['package-lock.json'][original_version] = new_version
+                    data['package-lock.json'][source.original] = new_version
 
                 for filename, script in scripts.items():
                     target = Path('$FLATPAK_BUILDER_BUILDDIR') / prefix / filename
