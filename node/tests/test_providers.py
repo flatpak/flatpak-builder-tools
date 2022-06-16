c6e9494d5f06b03fd41f1c9fcf95fa4f64d567bf
Ryan Gonzalez <ryan.gonzalez@collabora.com>
2022-06-15 19:22:43 -0500

    node: Run tests for multiple Node versions
    Also adds another useful tool shell script to be able to generate the
    lockfiles & examine the npm/yarn caches.
    
    Signed-off-by: Ryan Gonzalez <ryan.gonzalez@collabora.com>

--- node/tests/test_providers.py
+++ node/tests/test_providers.py
@@ -7,17 +7,18 @@ from flatpak_node_generator.manifest import ManifestGenerator
 async def test_special_electron(
     flatpak_builder: FlatpakBuilder,
     provider_factory_spec: ProviderFactorySpec,
+    node_version: int,
 ) -> None:
     with ManifestGenerator() as gen:
-        await provider_factory_spec.generate_modules('electron', gen)
+        await provider_factory_spec.generate_modules('electron', gen, node_version)
 
     flatpak_builder.build(
         sources=itertools.chain(gen.ordered_sources()),
         commands=[provider_factory_spec.install_command],
-        use_node=True,
+        use_node=node_version,
     )
 
     electron_version = (
         flatpak_builder.module_dir / 'node_modules' / 'electron' / 'dist' / 'version'
     )
-    assert electron_version.read_text() == '18.2.0'
+    assert electron_version.read_text() == '18.3.4'
