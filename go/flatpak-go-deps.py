d1559d94302776d8ce2e77d6073eeda3d04abb9e
Micah Lee <micah@micahflee.com>
2023-09-10 18:23:33 -0700

    Support extracting the commit id from the version string if it is there, and otherwise clone every git repo to find its commit id

--- go/flatpak-go-deps.py
+++ go/flatpak-go-deps.py
@@ -3,38 +3,82 @@ import subprocess
 import sys
 import json
 import os
+import re
 import tempfile
 import yaml
 import requests
 from bs4 import BeautifulSoup
 
 
-def get_module_info(module):
-    if " " not in module:
-        return None, None
+def extract_commit_id(module_version):
+    # Regex to match formats like: v0.0.0-20190819201941-24fa4b261c55
+    complex_format_regex = re.compile(
+        r"v\d+\.\d+\.\d+-\d{14}-(?P<commit>[a-fA-F0-9]{12,40})"
+    )
 
-    module_name, module_version = module.split(" ", 1)
+    match = complex_format_regex.search(module_version)
+    if match:
+        return match.group("commit")
+
+    # If the version is just a simple version like v1.4.0 or v0.13.0, return None
+    return None
+
+
+def get_commit_id_from_git(git_url, version=None):
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        try:
+            if version:
+                print(f"Cloning {git_url}@{version}...")
+                subprocess.run(
+                    ["git", "clone", "-b", version, git_url, tmp_dir], check=True
+                )
+            else:
+                print(f"Cloning {git_url}...")
+                subprocess.run(["git", "clone", git_url, tmp_dir], check=True)
+        except subprocess.CalledProcessError:
+            # If cloning with a specific tag fails, fall back to default branch
+            if version:
+                print(f"Tag {version} not found. Cloning {git_url} default branch...")
+                subprocess.run(["git", "clone", git_url, tmp_dir], check=True)
+
+        try:
+            result = subprocess.run(
+                ["git", "rev-parse", "HEAD"],
+                cwd=tmp_dir,
+                check=True,
+                capture_output=True,
+                text=True,
+            )
+            return result.stdout.strip()
+        except subprocess.CalledProcessError:
+            return None
+
+
+def get_module_info(module_name):
     result = subprocess.run(
         ["go", "list", "-m", "-json", module_name],
         check=True,
         capture_output=True,
         text=True,
     )
-    return module_name, json.loads(result.stdout)
+    return json.loads(result.stdout)
 
 
 def get_git_url(module_name):
+    # Remove the version suffix, if present
+    module_name = re.sub(r"/v\d+$", "", module_name)
+
     if "gitlab.com" in module_name:
         return (
-            f"https://gitlab.com/{module_name.lstrip('gitlab.com/')}"
+            f"https://gitlab.com/{module_name.replace('gitlab.com/', '')}"
             if module_name.endswith(".git")
-            else f"https://gitlab.com/{module_name.lstrip('gitlab.com/')}.git"
+            else f"https://gitlab.com/{module_name.replace('gitlab.com/', '')}.git"
         )
     elif "github.com" in module_name:
         return (
-            f"https://github.com/{module_name.lstrip('github.com/')}"
+            f"https://github.com/{module_name.replace('github.com/', '')}"
             if module_name.endswith(".git")
-            else f"https://github.com/{module_name.lstrip('github.com/')}.git"
+            else f"https://github.com/{module_name.replace('github.com/', '')}.git"
         )
     elif "git.torproject.org" in module_name:
         return f"https://{module_name}"
@@ -98,13 +142,18 @@ def main():
         )
 
         modules = result.stdout.strip().split("\n")
+        modules = modules[1:]  # Skip the first module, which is the current module
         sources = []
 
         for module in modules:
-            module_name, info = get_module_info(module)
-            if not module_name or not info:
-                continue
-            path = info["Path"]
+            module_name, module_version = module.split(" ", 1)
+            if module_version.endswith("+incompatible"):
+                module_version = module_version[:-13]
+
+            commit_id = extract_commit_id(module_version)
+
+            info = get_module_info(module_name)
+            path = info.get("Path")
             version = info.get("Version")
             if not version:
                 continue
@@ -113,16 +162,20 @@ def main():
             if not git_url:
                 git_url = f"https://{module_name}.git"
 
-            if version.startswith("v"):
-                ref_type = "tag"
-            else:
-                ref_type = "commit"
+            if not commit_id:
+                commit_id = get_commit_id_from_git(git_url, version)
+
+            if not commit_id:
+                print(
+                    f"Error: Could not retrieve commit ID for {module_name}@{version}."
+                )
+                continue
 
             sources.append(
                 {
                     "type": "git",
                     "url": git_url,
-                    ref_type: version,
+                    "commit": commit_id,
                     "dest": f"src/{path.replace('.', '/')}",
                 }
             )
