bf91cb0bee7ce0c8021e223e3ea9c5110ebb82de
bbhtt <bbhtt.zn0i8@slmail.me>
2025-05-04 09:02:53 +0530

    poetry: Prefer only any wheels
    Any platform or arch specific wheels is not guaranteed to work with
    the arches the Flatpak will be built on
    
    Fixes https://github.com/flatpak/flatpak-builder-tools/issues/396
    Fixes https://github.com/flatpak/flatpak-builder-tools/issues/394

--- poetry/flatpak-poetry-generator.py
+++ poetry/flatpak-poetry-generator.py
@@ -42,6 +42,7 @@ def get_pypi_source(name: str, version: str, hashes: list) -> tuple:
                 continue
             if (
                 source.get("packagetype") == "bdist_wheel"
+                and source.get("filename", "").endswith("-any.whl")
                 and "py3" in source.get("python_version", "")
             ):
                 matched = (source["url"], sha256)
