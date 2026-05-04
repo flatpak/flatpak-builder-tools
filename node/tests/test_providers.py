d50a3b0268ddd5d63e3d21b44c8e4aa3c414265d
bbhtt <bbhtt.zn0i8@slmail.me>
2026-05-04 07:00:10 +0530

    node: Fix symlink path for scoped packages
    Fixes: https://github.com/flatpak/flatpak-builder-tools/issues/521

--- node/tests/test_providers.py
+++ node/tests/test_providers.py
@@ -214,3 +214,22 @@ async def test_special_electron(
         flatpak_builder.module_dir / 'node_modules' / 'electron' / 'dist' / 'version'
     )
     assert electron_version.read_text() == '26.3.0'
+
+
+async def test_special_esbuild(
+    flatpak_builder: FlatpakBuilder,
+    npm_provider_factory_spec: ProviderFactorySpec,
+    node_version: int,
+) -> None:
+    with ManifestGenerator() as gen:
+        await npm_provider_factory_spec.generate_modules(
+            'esbuild-test', gen, node_version
+        )
+    flatpak_builder.build(
+        sources=itertools.chain(gen.ordered_sources()),
+        commands=[
+            npm_provider_factory_spec.install_command,
+            'flatpak-node/cache/esbuild/bin/esbuild-current --version',
+        ],
+        use_node=node_version,
+    )
